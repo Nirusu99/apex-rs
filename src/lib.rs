07b05aa3d307ac422946704f580b13a15bfc794b
Nils Pukropp <nils@nirusu.codes>
2022-08-15 08:38:52 +0200

    added fn for crafter rotations

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ mod http;
 pub mod model;
 
 const MAP_ROTATIONS_LOCATION: &'static str = "maprotation";
+const CRAFTER_ROTATIONS_LOCATION: &'static str = "crafting";
 const ALL_ROTATIONS: (&'static str, &'static str) = ("version", "2");
 
 #[derive(Debug)]
@@ -24,6 +25,14 @@ impl<'a> ApexClient<'a> {
         let body = http
             .request(MAP_ROTATIONS_LOCATION, &[ALL_ROTATIONS])
             .await?;
+        Ok(serde_json::from_str(&body)?)
+    }
+
+    pub async fn get_crafter_rotations(
+        &self,
+    ) -> Result<crate::model::Bundles, crate::error::ApexError> {
+        let http = Http::new_with_auth(self.token);
+        let body = http.request(CRAFTER_ROTATIONS_LOCATION, &[]).await?;
         Ok(serde_json::from_str(&body)?)
     }
 }
